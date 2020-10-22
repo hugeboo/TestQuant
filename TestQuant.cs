d131b8892313c341db61a787c12f47f49efa8534
Sergey SV <sesv@yandex.ru>
2020-10-23 02:43:29 +0300

    TestQuant template added

---
+++ TestQuant.cs
@@ -0,0 +1,19 @@
+﻿using QuantaBasket.Core.Contracts;
+using QuantaBasket.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantasBasket.Quantas.TestQuant
+{
+    public sealed class TestQuant : IQuant
+    {
+        public string Name => throw new NotImplementedException();
+
+        public HashSet<SecurityId> Securities => throw new NotImplementedException();
+
+        public IBasketService BasketService { set => throw new NotImplementedException(); }
+    }
+}
